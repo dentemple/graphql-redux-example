bafca1a93193b1158ea106225232fbf8391c748d
Den Temple <developer@dentemple.com>
2018-09-24 17:44:24 -0400

    Configure client and example route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.json([
+    {
+      id: 1,
+      username: 'test1'
+    },
+    {
+      id: 2,
+      username: 'test2'
+    }
+  ])
+})
 
-module.exports = router;
+module.exports = router
