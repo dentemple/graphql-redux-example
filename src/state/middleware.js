d3088d1406fb5b86abf9adc4ba8e4268d4bd457e
Den Temple <developer@dentemple.com>
2018-09-26 18:57:49 -0400

    Try custom middleware

---
+++ src/state/middleware.js
@@ -0,0 +1,27 @@
+export const logAction = store => next => action => {
+  console.log('ACTION', { action })
+  return next(action)
+}
+
+export const logResult = store => next => action => {
+  let result = next(action)
+  console.log('RESULT', { result })
+  return result
+}
+
+export const logStore = store => next => action => {
+  console.log('STORE', { getState: store.getState() })
+  return next(action)
+}
+
+export const vanillaPromise = store => next => action => {
+  if (typeof action.then !== 'function') {
+    return next(action)
+  }
+  return Promise.resolve(action).then(store.dispatch)
+}
+
+export const newThunk = store => next => action =>
+  typeof action === 'function'
+    ? action(store.dispatch, store.getState)
+    : next(action)
--- src/state/store.js
+++ src/state/store.js
@@ -1,14 +1,22 @@
 import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import { createLogger } from 'redux-logger'
+// import thunk from 'redux-thunk'
+// import { createLogger } from 'redux-logger'
 
 import reducers from './reducers'
+import {
+  logAction,
+  logResult,
+  logStore,
+  vanillaPromise,
+  newThunk
+} from './middleware'
 
 function configureStore(preloadedState) {
   const store = createStore(
     reducers,
     preloadedState,
-    applyMiddleware(thunk, createLogger())
+    // applyMiddleware(thunk, createLogger())
+    applyMiddleware(newThunk, logAction, logResult, logStore, vanillaPromise)
   )
 
   if (process.env.NODE_ENV !== 'production') {
